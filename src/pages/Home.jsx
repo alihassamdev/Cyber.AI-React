ebf759a896be2acef03a0ce10d7f8a7ca9f8fa54
alihassamdev <hassam.ali@studio93.io>
2025-08-20 19:27:27 +0500

    feat(footer): add footer component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Home = () => {
     return (
-        <Header />
+        <>
+            <Header />
+            <Footer />
+        </>
+
     )
 }
 
