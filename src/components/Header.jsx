073c9ee4a906be622c6951c7f745f435967f7191
alihassamdev <hassam.ali@studio93.io>
2025-08-22 07:37:05 +0500

    added mobile menu to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Header.css'
 
@@ -6,28 +6,63 @@ import unionIconYellow from '../assets/icons/Union-yellow.png';
 import unionIconRed from '../assets/icons/Union-red.png';
 
 const Header = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => setMenuOpen(!menuOpen);
+
+    useEffect(() => {
+        if (menuOpen) {
+            document.body.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+        }
+    }, [menuOpen]);
+
     return (
         <header>
-            <div className='container header-container'>
-                <div><span className='logo gradient-text'>Cyber.AI</span></div>
-                <nav>
+            <div className="container header-container">
+                <div className="logo-box">
+                    <span className="logo gradient-text">Cyber.AI</span>
+                </div>
+
+                <nav className="desktop-nav">
                     <ul>
-                        <li><a href='#'>Home</a></li>
-                        <li><a href='#'>Pages</a></li>
-                        <li><a href='#'>Support</a></li>
-                        <li><a href='#'>Contact</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Pages</a></li>
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Contact</a></li>
                     </ul>
                 </nav>
-                <div><button className='button button-primary'>Get Started Free</button></div>
+
+                <div className="desktop-cta">
+                    <button className="button button-primary">Get Started Free</button>
+                </div>
+
+                {/* Toggle Button for Mobile */}
+                <button className="menu-toggle" onClick={toggleMenu}>
+                    ☰
+                </button>
             </div>
-            <div className='header-bottom'>
-                <div className='header-bottom-container'>
+
+            {/* Mobile Menu Panel */}
+            <div className={`mobile-menu ${menuOpen ? 'open' : ''}`}>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Pages</a></li>
+                    <li><a href="#">Support</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <button className="button button-primary">Get Started Free</button>
+            </div>
+
+            <div className="header-bottom">
+                <div className="header-bottom-container">
                     <img src={unionIconRed} alt="" />
                     <img src={unionIconYellow} alt="" />
                 </div>
             </div>
-
         </header>
+
     )
 }
 
